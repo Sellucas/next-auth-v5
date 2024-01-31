7448bd0e0d738db6cf7f9a90cab434d6d996a764
Lucas Sell <lucassellmachado@gmail.com>
2024-01-31 14:15:50 -0300

    feat: signIn and signOut

--- actions/register.ts
+++ actions/register.ts
@@ -1,20 +1,20 @@
 "use server";
 
-import bcrypt from "bcrypt";
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 
-import { RegisterSchema } from "@/schemas";
 import { db } from "@/lib/db";
+import { RegisterSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-  const valideFields = RegisterSchema.safeParse(values);
-
-  if (!valideFields.success) {
+  const validatedFields = RegisterSchema.safeParse(values);
+  
+  if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
 
-  const { email, password, name } = valideFields.data;
+  const { email, password, name } = validatedFields.data;
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const existingUser = await getUserByEmail(email);
--- auth.ts
+++ auth.ts
@@ -1,9 +1,15 @@
-import NextAuth from "next-auth";
+import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter";
+
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 
-export const { handlers, auth } = NextAuth({
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
