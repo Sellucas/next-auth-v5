465e0a9d28c8fc07a55416f00706bf70421f8686
Lucas Sell <lucassellmachado@gmail.com>
2024-02-02 15:50:05 -0300

    feat: Send email verification

--- auth.ts
+++ auth.ts
@@ -25,6 +25,19 @@ export const {
     },
   },
   callbacks: {
+    async signIn({ user, account }) {
+      // Allow OAuth without email verification
+      if (account?.provider !== "credentials") return true;
+
+      // Prevent sign in whitout email verification
+      const existingUser = await getUserById(user.id);
+
+      if (!existingUser?.emailVerified) return false;
+
+      // TODO: Add 2FA check
+
+      return true;
+    },
     async session({ session, token }: { session: Session; token?: any }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
