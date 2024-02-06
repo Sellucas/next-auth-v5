92f1b7acde4296728571f5b8d0419b9e888bef05
Lucas Sell <lucassellmachado@gmail.com>
2024-02-06 15:04:58 -0300

    feat: 2FA

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { UserRole } from "@prisma/client";
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 import { getUserById } from "@/data/user";
+import { getTwoFactorConfirmationByUserId } from "./data/tw-factor-confirmation";
 
 export const {
   handlers: { GET, POST },
@@ -34,7 +35,18 @@ export const {
 
       if (!existingUser?.emailVerified) return false;
 
-      // TODO: Add 2FA check
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id
+        );
+
+        if (!twoFactorConfirmation) return false;
+
+        // Delete two factor confirmation for next sign in
+        await db.twoFactorConfirmation.delete({
+          where: { id: twoFactorConfirmation.id },
+        });
+      }
 
       return true;
     },
